c62d515d24e5206848a33d2afefdaa0c007d4e4e
mujumuju <55851387+mujumuju@users.noreply.github.com>
2019-09-27 18:48:32 +0900

    Update nakobot.py

--- nakobot.py
+++ nakobot.py
@@ -1,6 +1,6 @@
 # インストールした discord.py を読み込む
 import discord
-import random
+#import random
 
 # 自分のBotのアクセストークンに置き換えてください
 TOKEN = 'NTA0Njg4ODA5ODQ2NTA1NDcz.XYzHgQ.2pqbM7tfQnUk9Ydzg-cunbx03LY'
@@ -24,15 +24,15 @@ async def on_message(message):
         return
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '！なこ':
-	random.uniform(0,3)
-	if random.uniform == 0
+	#random.uniform(0,3)
+	#if random.uniform == 0
         await message.channel.send('ウホウホ')
-if random.uniform == 1
-        await message.channel.send('おはなっこ！！！！！！')
-if random.uniform == 2
-        await message.channel.send('ふーん')
-if random.uniform == 3
-        await message.channel.send('腹筋われた！！！！！！！')
+#if random.uniform == 1
+       # await message.channel.send('おはなっこ！！！！！！')
+#if random.uniform == 2
+       # await message.channel.send('ふーん')
+#if random.uniform == 3
+       # await message.channel.send('腹筋われた！！！！！！！')
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
