167671f32c6e5400e5498222263d406f920ff004
mujumuju <55851387+mujumuju@users.noreply.github.com>
2019-09-27 23:30:45 +0900

    Update nakobot.py

--- nakobot.py
+++ nakobot.py
@@ -1,15 +1,14 @@
-import discord:
-TOKEN = 'NTA0Njg4ODA5ODQ2NTA1NDcz.XYzHgQ.2pqbM7tfQnUk9Ydzg-cunbx03LY':
-client = discord.Client():
+import discord
+TOKEN = 'NTA0Njg4ODA5ODQ2NTA1NDcz.XY4Sgw.xqEsYGcnIJGjGHUtiRdkiBkTZuk'
+client = discord.Client()
 
-@client.event
-async def on_ready():
 
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
-    if message.content == '！なこ':
-        await message.channel.send('ウホウホ')
-	
+    if message.content == "！なこ":
+        await message.channel.send("ウホウホッ！")
+
 client.run(TOKEN)
+
